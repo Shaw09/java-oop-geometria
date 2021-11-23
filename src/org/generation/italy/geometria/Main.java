39b4a35973c8e3b930a0c097b0f8f9f1a647973e
Shaw <michele_aliffi@yahoo.it>
2021-11-23 17:25:04 +0100

    Aggiornati i file, fine progetto.

--- src/org/generation/italy/geometria/Main.java
+++ src/org/generation/italy/geometria/Main.java
@@ -1,5 +1,27 @@
 package org.generation.italy.geometria;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int base;
+		int altezza;
+	
+		System.out.print("Inserire qui la base del tuo rettangolo: ");
+		base = scanner.nextInt();
+		
+		System.out.print("Inserire qui l`altezza del tuo rettangolo: ");
+		altezza = scanner.nextInt();
+		
+		Rettangolo rettangolo = new Rettangolo(base, altezza);
+		
+	
+		System.out.println("Il tuo perimetro è di: " + rettangolo.calcolaPerimetro());
+		System.out.println("La tua area è di: " + rettangolo.calcolaArea());
+		
+		scanner.close();
+	}
 }
--- src/org/generation/italy/geometria/Rettangolo.java
+++ src/org/generation/italy/geometria/Rettangolo.java
@@ -2,4 +2,20 @@ package org.generation.italy.geometria;
 
 public class Rettangolo {
 
+	int base;
+	int altezza;
+	
+	Rettangolo(int base, int altezza) {
+		this.base = base;
+		this.altezza = altezza;
+	}
+	
+	int calcolaPerimetro() {
+		return this.base * 2 + this.altezza * 2;
+	}
+	int calcolaArea() {
+		return this.base * this.altezza;
+	}
+	
+	
 }
